3ca7211fcf73e8ea763b9a4d9f90687c35831df5
Tao Song <stmymail@pku.edu.cn>
2021-06-13 22:02:04 +0800

    finish api MNSIM

--- mora/HW.py
+++ mora/HW.py
@@ -10,11 +10,12 @@ import MNSIM
 
 
 class DLA(object):
-    def __init__(self, hw_param_dicts, dataflow, maestro_run_path) -> None:
+    def __init__(self, hw_param_dicts, dataflow, home_path) -> None:
         super(DLA, self).__init__()
         self.hw_param_dicts = hw_param_dicts
         self.dataflow = dataflow
-        self.maestro_run_path = maestro_run_path
+        self.home_path = home_path
+        self.maestro_run_path = os.path.abspath(os.path.join(home_path, "maestro_run.sh"))
         self.dla_dicts = self.set_dla()
 
     def set_dla(self):
@@ -27,21 +28,42 @@ class DLA(object):
         dla_dicts['dataflow'] = self.dataflow
         return dla_dicts
 
+    def invoke_maestro(self, model):
+
+        return
+
 
 class RRAM(object):
-    def __init__(self, hw_param_dicts, rram_config_path=os.path.dirname(__file__)) -> None:
+    def __init__(self, hw_param_dicts, home_path) -> None:
         super(RRAM, self).__init__()
         self.hw_param_dicts = hw_param_dicts
-        self.rram_config_path = rram_config_path
         self.rram_dicts = self.set_rram()
-        self.mem_capacity = self.get_memory_capacity()  # bit
+        self.mem_capacity = self.get_memory_capacity()
+        self.home_path = home_path
+        self.rram_config_path = os.path.abspath(os.path.join(home_path, "rram_config.ini"))
 
     def set_rram(self):
         rram_dicts = {}
-        rram_dicts['tiles'] = self.hw_param_dicts['rram_tiles']
+        rram_dicts['tile_row'] = self.hw_param_dicts['rram_tile_size']
+        rram_dicts['tile_col'] = self.hw_param_dicts['rram_tile_size']
         rram_dicts['glb_size'] = self.hw_param_dicts['glb_size']
         rram_dicts['tile_bw'] = self.hw_param_dicts['rram_tile_bw']
         return rram_dicts
 
     def get_memory_capacity(self):
-        return self.rram_dicts['tiles'] * 16 * 8 * 2 * 128**2
+        return self.rram_dicts['tile_size']**2 * 16 * 8 * 2 * 128**2  # bit
+
+    def invoke_MNSIM(self, model):
+        output_csv_path = os.path.abspath(os.path.join(self.home_path, 'output/' + model + '/' + model + '-rram.csv'))
+        if os.path.exists(output_csv_path):
+            print("rram outfile conflict.")
+            raise AttributeError
+        command = ["python ../MNSIM.py -model {v1} -tile_size {v2} -tile_noc_bw {v3}".format(v1=model, v2=[self.rram_dicts['tile_row'], self.rram_dicts['tile_col']], v3=self.rram_dicts['tile_bw'])]
+        process = SP.Popen(command, stdout=SP.PIPE, stderr=SP.PIPE)
+        stdout, stderr = process.communicate()
+        process.wait()
+        if os.path.exists(output_csv_path):
+            print('MNSIM invoked', self.rram_dicts)
+        else:
+            print('MNSIM invoke fatal.')
+            raise AttributeError
--- mora/api.py
+++ mora/api.py
@@ -9,19 +9,28 @@ import torch
 import MNSIM
 
 
-def EDP(DLA, RRAM, model, maestro_run_path):
-    return {'dla': invoke_maestro(DLA, model, maestro_run_path).get_edp(), 'rram': invoke_MNSIM(RRAM, model).get_edp()}
+def EDP(DLA, RRAM, model, home_path):
+    # return {'dla': DLA.get_edp(model), 'rram': RRAM.get_edp(model)}
+    dla_output_csv_path = os.path.abspath(os.path.join(home_path, 'output/' + model + '/' + model + '-dla.csv'))
+    rram_output_csv_path = os.path.abspath(os.path.join(home_path, 'output/' + model + '/' + model + '-rram.csv'))
+    if os.path.exists(dla_output_csv_path) is not True or os.path.exists(dla_output_csv_path) is not True:
+        print("api.read outfile conflict.")
+        raise AttributeError
+    dla_out = pd.read_csv(dla_output_csv_path)
+    rram_out = pd.read_csv(rram_output_csv_path)
+    dla_edp = float(dla_out.at[0, 'energy']) * float(dla_out.at[0, 'latency'])
+    rram_edp = float(rram_out.at[0, 'energy']) * float(rram_out.at[0, 'latency'])
+    return {'dla': dla_edp, 'rram': rram_edp}
 
 
-def area(DLA, RRAM, model, maestro_run_path):
-    return {'dla': invoke_maestro(DLA, model).get_area(), 'rram': invoke_MNSIM(RRAM, model).get_area()}
-
-
-class invoke_maestro(object):
-    def __init__(self, DLA, model, maestro_run_path) -> None:
-        super(invoke_maestro, self).__init__()
-
-
-class invoke_MNSIM(object):
-    def __init__(self, DLA, model, maestro_run_path) -> None:
-        super(invoke_MNSIM, self).__init__()
+def area(DLA, RRAM, model, home_path):
+    dla_output_csv_path = os.path.abspath(os.path.join(home_path, 'output/' + model + '/' + model + '-dla.csv'))
+    rram_output_csv_path = os.path.abspath(os.path.join(home_path, 'output/' + model + '/' + model + '-rram.csv'))
+    if os.path.exists(dla_output_csv_path) is not True or os.path.exists(dla_output_csv_path) is not True:
+        print("api.read outfile conflict.")
+        raise AttributeError
+    dla_out = pd.read_csv(dla_output_csv_path)
+    rram_out = pd.read_csv(rram_output_csv_path)
+    dla_area = float(dla_out.at[0, 'area'])
+    rram_area = float(rram_out.at[0, 'area'])
+    return {'dla': dla_area, 'rram': rram_area}
